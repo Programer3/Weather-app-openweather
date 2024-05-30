2bdb3801933b744383fec24d6eaccdeb302d1c56
Aman Gupta <4programmimg@gmail.com>
2024-05-30 16:07:46 +0530

    bruh testing again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // require('dotenv').config();
 // const apiKey = process.env.API_KEY;
 // const apiKey = '{{ secrets.API_KEY }}';
-const apiKey = window.ENV.API_KEY;
+const apiKey = 'API_KEY_PLACEHOLDER';
 const weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
 const iconUrl = "https://openweathermap.org/img/wn/";
 const weatherIcon = document.querySelector(".weather-icon");
